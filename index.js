e8ebf0b134effae92223621fee022d11400b682b
AnNguyenLeMT <annguyenlemt@gmail.com>
2024-04-09 10:42:18 +0700

    adding redis config

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const expressHbs = require("express-handlebars");
 const { createPagination } = require("express-handlebars-paginate");
 
 const session = require("express-session");
+const redisStore = require("connect-redis").default;
+const { createClient } = require("redis");
+const redisClient = createClient({
+	// url: "rediss://red-coabb14f7o1s73dnb4vg:S4hNdW4cBSZOjb6t6szL1OaJzFyFx1pl@singapore-redis.render.com:6379",
+	url: "redis://red-coabb14f7o1s73dnb4vg:6379",
+});
+redisClient.connect().catch(console.error);
 
 const app = express();
 const port = process.env.port || 3000;
@@ -29,16 +36,17 @@ app.engine(
 app.set("view engine", "hbs");
 
 app.use(express.json());
-app.use(express.urlencoded({urlencoded: false}))
+app.use(express.urlencoded({ urlencoded: false }));
 
 app.use(
 	session({
 		secret: "S3cret",
+		store: new redisStore({ client: redisClient }),
 		resave: false,
 		saveUninitialized: false,
 		cookie: {
 			httpOnly: true,
-			maxAge: 20*60*1000, // 20 minutes
+			maxAge: 20 * 60 * 1000, // 20 minutes
 		},
 	})
 );
@@ -46,13 +54,13 @@ app.use(
 app.use((req, res, next) => {
 	let Cart = require("./controllers/cart");
 	req.session.cart = new Cart(req.session.cart ? req.session.cart : {});
-	res.locals.quantity = req.session.cart.quantity
+	res.locals.quantity = req.session.cart.quantity;
 	next();
-})
+});
 
 app.use("/products", require("./routes/productsRouter"));
 
-app.use("/users", require('./routes/usersRouter'));
+app.use("/users", require("./routes/usersRouter"));
 
 app.use("/", require("./routes/indexRouter"));
 
